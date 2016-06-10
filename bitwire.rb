6ac99079d41145bdc20918077d10ad717d8acecf
Patryk Dwórznik <patryk@dworznik.com>
2016-06-10 16:52:00 +0700

    Bump version number to 0.0.2

--- bitwire.rb
+++ bitwire.rb
@@ -1,7 +1,7 @@
 class Bitwire < Formula
   homepage "https://github.com/dworznik/bitwire"
-  url "https://github.com/dworznik/bitwire/releases/download/v0.0.1/bitwire.tar.gz"
-  sha256 "d6646d29a13e3eda0d160c5ada3e64a204e9fd8b355c6f66c40b5b2618a9cb62"
+  url "https://github.com/dworznik/bitwire/releases/download/v0.0.2/bitwire-0.0.2.tar.gz"
+  sha256 "8d183f4711a62d4097ac79a5355a4d5a75b89474"
   version "0.0.1"
 
   def install
