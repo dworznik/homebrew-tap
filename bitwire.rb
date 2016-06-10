11a0c9206e591119d4c6a537f163678d53df95d3
Patryk Dwórznik <patryk@dworznik.com>
2016-06-10 16:55:08 +0700

    Fix the version number

--- bitwire.rb
+++ bitwire.rb
@@ -1,8 +1,8 @@
 class Bitwire < Formula
   homepage "https://github.com/dworznik/bitwire"
   url "https://github.com/dworznik/bitwire/releases/download/v0.0.2/bitwire-0.0.2.tar.gz"
-  sha256 "8d183f4711a62d4097ac79a5355a4d5a75b89474"
-  version "0.0.1"
+  sha256 "742b3971d63bd2de8b567e6341ccfe0156aef544fd8dbc0fbc051fbf6c9da7ec"
+  version "0.0.2"
 
   def install
     bin.install "bitwire"
